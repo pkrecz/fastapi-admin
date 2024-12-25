bc6364becd73fcf42a6e4e80056d155755a7d9d2
pkrecz <pkrecz@poczta.onet.pl>
2024-12-25 17:00:34 +0100

    Error handling changes

--- app_admin/service.py
+++ app_admin/service.py
@@ -24,82 +24,63 @@ class AuthenticationService:
 
 
     def authentication_register_user(self, data: BaseModel) -> Model:
-        try:
-            if self.auth.check_if_exists_user_by_username(data.username):
-                raise exceptions.UserExistsException
-            if self.auth.check_if_exists_user_by_email(data.email):
-                raise exceptions.EmailExistsException
-            if self.auth.check_the_same_password(data.password, data.password_confirm) == False:
-                raise exceptions.NotTheSamePasswordException
-            input = {
-                "username": data.username,
-                "full_name": data.full_name,
-                "email": data.email,
-                "hashed_password": self.auth.hash_password(data.password)}
-            instance = self.model(**input)
-            return self.crud.create(instance)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        if self.auth.check_if_exists_user_by_username(data.username):
+            raise exceptions.UserExistsException
+        if self.auth.check_if_exists_user_by_email(data.email):
+            raise exceptions.EmailExistsException
+        if self.auth.check_the_same_password(data.password, data.password_confirm) == False:
+            raise exceptions.NotTheSamePasswordException
+        input = {
+            "username": data.username,
+            "full_name": data.full_name,
+            "email": data.email,
+            "hashed_password": self.auth.hash_password(data.password)}
+        return self.crud.create(input)
 
 
     def authentication_update_user(self, data: BaseModel) -> Model:
-        try:
-            instance = self.auth.get_user_by_username(self.cuser.username)
-            if not instance:
-                raise exceptions.UserNotFoundException
-            return self.crud.update(instance, data)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        instance = self.auth.get_user_by_username(self.cuser.username)
+        if not instance:
+            raise exceptions.UserNotFoundException
+        return self.crud.update(instance, data)
 
 
     def authentication_delete_user(self):
-        try:
-            instance = self.auth.get_user_by_username(self.cuser.username)
-            if not instance:
-                raise exceptions.UserNotFoundException
-            if not self.crud.delete(instance):
-                raise
-            return JSONResponse(content={"message": "User deleted successfully."}, status_code=status.HTTP_200_OK)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        instance = self.auth.get_user_by_username(self.cuser.username)
+        if not instance:
+            raise exceptions.UserNotFoundException
+        if not self.crud.delete(instance):
+            raise
+        return JSONResponse(content={"message": "User deleted successfully."}, status_code=status.HTTP_200_OK)
 
 
     def authentication_change_password(self, data: BaseModel):
-        try:
-            instance = self.auth.get_user_by_username(self.cuser.username)
-            if not instance:
-                raise exceptions.UserNotFoundException
-            if self.auth.check_the_same_password(data.new_password, data.new_password_confirm) == False:
-                raise exceptions.NotTheSamePasswordException
-            if self.auth.verify_password(data.old_password, instance.hashed_password) == False:
-                raise exceptions.IncorrectPasswordException
-            data = {"hashed_password": self.auth.hash_password(data.new_password)}
-            self.crud.update(instance, data)
-            return JSONResponse(content={"message": "Password changed successfully."}, status_code=status.HTTP_200_OK)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        instance = self.auth.get_user_by_username(self.cuser.username)
+        if not instance:
+            raise exceptions.UserNotFoundException
+        if self.auth.check_the_same_password(data.new_password, data.new_password_confirm) == False:
+            raise exceptions.NotTheSamePasswordException
+        if self.auth.verify_password(data.old_password, instance.hashed_password) == False:
+            raise exceptions.IncorrectPasswordException
+        data = {"hashed_password": self.auth.hash_password(data.new_password)}
+        self.crud.update(instance, data)
+        return JSONResponse(content={"message": "Password changed successfully."}, status_code=status.HTTP_200_OK)
 
 
     def authentication_login(self, data: OAuth2PasswordBearer):
-        try:
-            user = self.auth.authenticate_user(data.username, data.password)
-            if not user:
-                raise exceptions.CredentialsException
-            if self.auth.get_active_status(user.username) == False:
-                raise exceptions.UserInActiveException
-            access_token = self.auth.create_token(data={"sub": user.username}, refresh=False)
-            refresh_token = self.auth.create_token(data={"sub": user.username}, refresh=True)
-            return JSONResponse(content={"access_token": access_token, "refresh_token": refresh_token}, status_code=status.HTTP_200_OK)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        user = self.auth.authenticate_user(data.username, data.password)
+        if not user:
+            raise exceptions.CredentialsException
+        if self.auth.get_active_status(user.username) == False:
+            raise exceptions.UserInActiveException
+        access_token = self.auth.create_token(data={"sub": user.username}, refresh=False)
+        refresh_token = self.auth.create_token(data={"sub": user.username}, refresh=True)
+        return JSONResponse(content={"access_token": access_token, "refresh_token": refresh_token}, status_code=status.HTTP_200_OK)
 
 
     def authentication_refresh(self):
-        try:
-            access_token = self.auth.create_token(data={"sub": self.cuser.username}, refresh=False)
-            return JSONResponse(content={"access_token": access_token}, status_code=status.HTTP_200_OK)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        access_token = self.auth.create_token(data={"sub": self.cuser.username}, refresh=False)
+        return JSONResponse(content={"access_token": access_token}, status_code=status.HTTP_200_OK)
 
 
 class AuthorizationService:
@@ -114,118 +95,91 @@ class AuthorizationService:
 
 
     def authorization_create_role(self, data: BaseModel) -> Model:
-        try:
-            if self.authorization_role.check_if_exists_role_by_name(data.name):
-                raise exceptions.BadRequestException("Role already exists.")
-            instance = RoleModel(**data.model_dump())
-            return self.crud.create(instance)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        if self.authorization_role.check_if_exists_role_by_name(data.name):
+            raise exceptions.BadRequestException("Role already exists.")
+        input = data.model_dump()
+        return self.crud.create(input)
 
 
     def authorization_assign_role_to_user(self, username: str, role: str):
+        user = self.authentication_user.get_user_by_username(username)
+        role = self.authorization_role.get_role_by_name(role)
+        if not user or not role:
+            raise exceptions.NotFoundException("User or role was not found.")
         try:
-            user = self.authentication_user.get_user_by_username(username)
-            role = self.authorization_role.get_role_by_name(role)
-            if not user or not role:
-                raise exceptions.NotFoundException("User or role was not found.")
-            try:
-                queue = user
-                queue.roles.append(role)
-                self.db.flush()
-            except:
-                raise exceptions.BadRequestException("Assigment already exists.")
-            return JSONResponse(content={"message": "Role assigned to user."}, status_code=status.HTTP_200_OK)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+            queue = user
+            queue.roles.append(role)
+            self.db.flush()
+        except:
+            raise exceptions.BadRequestException("Assigment already exists.")
+        return JSONResponse(content={"message": "Role assigned to user."}, status_code=status.HTTP_200_OK)
 
 
     def authorization_unassign_role_from_user(self, username: str, role: str):
+        user = self.authentication_user.get_user_by_username(username)
+        role = self.authorization_role.get_role_by_name(role)
+        if not user or not role:
+            raise exceptions.NotFoundException("User or role was not found.")
         try:
-            user = self.authentication_user.get_user_by_username(username)
-            role = self.authorization_role.get_role_by_name(role)
-            if not user or not role:
-                raise exceptions.NotFoundException("User or role was not found.")
-            try:
-                queue = user
-                queue.roles.remove(role)    
-                self.db.flush()
-            except:
-                raise exceptions.BadRequestException("Assigment was not found.")
-            return JSONResponse(content={"message": "Role unassigned from user."}, status_code=status.HTTP_200_OK)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+            queue = user
+            queue.roles.remove(role)    
+            self.db.flush()
+        except:
+            raise exceptions.BadRequestException("Assigment was not found.")
+        return JSONResponse(content={"message": "Role unassigned from user."}, status_code=status.HTTP_200_OK)
 
 
     def authorization_get_role_per_user(self, username: str):
-        try:
-            user = self.authentication_user.get_user_by_username(username)
-            if not user:
-                raise exceptions.UserNotFoundException
-            return user
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        user = self.authentication_user.get_user_by_username(username)
+        if not user:
+            raise exceptions.UserNotFoundException
+        return user
 
 
     def authorization_get_user_per_role(self, role: str):
-        try:
-            role = self.authorization_role.get_role_by_name(role)
-            if not role:
-                raise exceptions.NotFoundException("Role was not found.")
-            return role
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        role = self.authorization_role.get_role_by_name(role)
+        if not role:
+            raise exceptions.NotFoundException("Role was not found.")
+        return role
 
 
     def authorization_assign_permission_to_role(self, role: str, permission: str):
+        role = self.authorization_role.get_role_by_name(role)
+        permission = self.authorization_permission.get_permission_by_name(permission)
+        if not role or not permission:
+            raise exceptions.NotFoundException("Role or permission was not found.")
         try:
-            role = self.authorization_role.get_role_by_name(role)
-            permission = self.authorization_permission.get_permission_by_name(permission)
-            if not role or not permission:
-                raise exceptions.NotFoundException("Role or permission was not found.")
-            try:
-                queue = role
-                queue.permissions.append(permission)
-                self.db.flush()
-            except:
-                raise exceptions.BadRequestException("Assigment already exists.")
-            return JSONResponse(content={"message": "Permission assigned to role."}, status_code=status.HTTP_200_OK)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+            queue = role
+            queue.permissions.append(permission)
+            self.db.flush()
+        except:
+            raise exceptions.BadRequestException("Assigment already exists.")
+        return JSONResponse(content={"message": "Permission assigned to role."}, status_code=status.HTTP_200_OK)
 
 
     def authorization_unassign_permission_from_role(self, role: str, permission: str):
+        role = self.authorization_role.get_role_by_name(role)
+        permission = self.authorization_permission.get_permission_by_name(permission)
+        if not role or not permission:
+            raise exceptions.NotFoundException("Role or permission was not found.")
         try:
-            role = self.authorization_role.get_role_by_name(role)
-            permission = self.authorization_permission.get_permission_by_name(permission)
-            if not role or not permission:
-                raise exceptions.NotFoundException("Role or permission was not found.")
-            try:
-                queue = role
-                queue.permissions.remove(permission)
-                self.db.flush()
-            except:
-                raise exceptions.BadRequestException("Assigment was not found.")
-            return JSONResponse(content={"message": "Permission unassigned from role."}, status_code=status.HTTP_200_OK)
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+            queue = role
+            queue.permissions.remove(permission)
+            self.db.flush()
+        except:
+            raise exceptions.BadRequestException("Assigment was not found.")
+        return JSONResponse(content={"message": "Permission unassigned from role."}, status_code=status.HTTP_200_OK)
 
 
     def authorization_get_permission_per_user(self, username: str):
-        try:
-            user = self.authentication_user.get_user_by_username(username)
-            if not user:
-                raise exceptions.UserNotFoundException
-            return user
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        user = self.authentication_user.get_user_by_username(username)
+        if not user:
+            raise exceptions.UserNotFoundException
+        return user
 
 
     def authorization_get_all_permission(self):
-        try:
-            permission = self.authorization_permission.get_all_permission()
-            if not permission:
-                raise exceptions.BadRequestException("No permission found.")
-            return permission
-        except Exception as exception:
-            return JSONResponse(content={"detail": exception.detail}, status_code=exception.status_code)
+        permission = self.authorization_permission.get_all_permission()
+        if not permission:
+            raise exceptions.BadRequestException("No permission found.")
+        return permission
